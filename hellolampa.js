34623712c64de7a5d127ac993fa25dc108212234
shamilmusaev <71009992+shamilmusaev@users.noreply.github.com>
2025-05-06 18:48:53 +0200

    Update hellolampa.js

--- hellolampa.js
+++ hellolampa.js
@@ -1,18 +1,22 @@
-(function waitForLampa() {
-  if (typeof window.Lampa === 'undefined' || typeof Lampa.Plugin === 'undefined') {
-    return setTimeout(waitForLampa, 500);
+(function wait() {
+  if (typeof Lampa === 'undefined' || typeof Lampa.Listener === 'undefined') {
+    return setTimeout(wait, 300);
   }
 
-  try {
-    Lampa.Plugin.register('hello_world', {
-      title: 'Hello World',
-      version: '1.0',
-      description: 'Простой тестовый плагин для LAMPA',
-      run() {
-        Lampa.Noty.show('Привет из моего плагина!');
+  Lampa.Listener.follow('app', function (e) {
+    if (e.type === 'ready') {
+      try {
+        Lampa.Plugin.register('hello_world', {
+          title: 'Hello World',
+          version: '1.0',
+          description: 'Простой тестовый плагин для LAMPA',
+          run() {
+            Lampa.Noty.show('Привет из моего плагина!');
+          }
+        });
+      } catch (e) {
+        console.error('Ошибка в плагине hello_world:', e);
       }
-    });
-  } catch (e) {
-    console.error('Ошибка в плагине hello_world:', e);
-  }
+    }
+  });
 })();
